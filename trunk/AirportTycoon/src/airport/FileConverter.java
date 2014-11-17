4cab5c93234d99c5f622ff1f6bcd97bb6a5a859a
stoian_ivanov_ivanov@abv.bg <stoian_ivanov_ivanov@abv.bg@e90669fe-beee-4d1e-0aca-1d4f0396343d>
2014-11-17 07:46:22 +0000

    ...

--- trunk/AirportTycoon/src/airport/FileConverter.java
+++ trunk/AirportTycoon/src/airport/FileConverter.java
@@ -1,34 +1,39 @@
 package airport;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-public class FileConvert {
-
 	public class FileConverter {
 
-		private static final String FILE_NAME="trafficSheet";
+		private static final String FILE_NAME = "src/trafficSheet.txt";
 		private Queue<String> messages ;
-		private AirportInfo airportIfno;
+		private AirportInfo airportInfo;
 		
 		public FileConverter() {
 				
 				try {
 					InputStream file= new FileInputStream(FILE_NAME);
-					Scanner sc= new Scanner(file);
-					Integer runwayCapacity=sc.nextInt();
-					Integer airplanesCapacity=sc.nextInt();
-					airportIfno= new AirportInfo(runwayCapacity, airplanesCapacity);
+					Scanner sc = new Scanner(file);
+					
+					Integer runwayCapacity = sc.nextInt();
+					Integer airplanesCapacity = sc.nextInt();
+					Integer movementTime = sc.nextInt();
+					
+					airportInfo= new AirportInfo(runwayCapacity, airplanesCapacity, movementTime);
+					
+					messages = new LinkedList<String>();
+					String str1 = sc.nextLine();
 					while(sc.hasNext()){
-						String str=sc.nextLine();
+						String str = sc.nextLine();
 						initQueue(str);
 					}
 					file.close();
+					sc.close();
 				} catch (FileNotFoundException e) {
 					e.printStackTrace();
 				} catch (IOException e) {
@@ -37,7 +42,7 @@ public class FileConvert {
 				
 		}
 		
-		private  void initQueue(String message){
+		private void initQueue(String message){
 			
 			messages.add(message);
 		}
@@ -45,11 +50,12 @@ public class FileConvert {
 			return messages;
 		}
 		public AirportInfo getAirportInfo(){		
-			return airportIfno;
+			return airportInfo;
 		}
 		class AirportInfo {
 			private Integer runwayCapacity ; 
 			private Integer airplanesCapacity;
+			private Integer movementTime; 
 			
 			public Integer getRunwayCapacity(){
 				return runwayCapacity;
@@ -63,10 +69,17 @@ public class FileConvert {
 			public void setAirplanesCapacity(Integer airplanesCapacity){
 				this.airplanesCapacity=airplanesCapacity;
 			}
-			public AirportInfo(Integer runwayCapacity, Integer airplanesCapacity) {
+			public Integer getMovementTime() {
+				return movementTime;
+			}
+			public void setMovementTime(Integer movementTime) {
+				this.movementTime = movementTime;
+			}
+			public AirportInfo(Integer runwayCapacity, Integer airplanesCapacity, Integer movementTime) {
 				this.runwayCapacity = runwayCapacity;
 				this.airplanesCapacity = airplanesCapacity;
+				this.movementTime = movementTime;
 			}
 		}
 	}
-}
+
